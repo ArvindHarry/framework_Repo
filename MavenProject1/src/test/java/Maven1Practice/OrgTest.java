86daafe650533e8c2b0e11ba38b9ac50f0320ddf
Priya <Priya@DESKTOP-O2DKLUF>
2025-03-03 16:47:23 +0530

    First Push

---
+++ MavenProject1/src/test/java/Maven1Practice/OrgTest.java
@@ -0,0 +1,16 @@
+package Maven1Practice;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+		@Test
+		public void createOrgTest() {
+			System.out.println("Execute createOrgTest");
+		}
+		
+		@Test
+		public void modifyOrgTest() {
+			System.out.println("Execute modifyOrgTest");
+		}
+
+}
